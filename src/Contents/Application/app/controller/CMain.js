243324ff5a4135435dfc2ff79908da6a78025614
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2015-06-09 14:35:30 +0200

    Update# 7

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -14,6 +14,9 @@ App.controller.define('CMain', {
 		{
 			"VMain" : {
 				show: "VMain_onShow"
+			},
+			"panel#scanner" : {
+				tap: "scanner_onclick"
 			}
 		},
 		
@@ -22,13 +25,29 @@ App.controller.define('CMain', {
 	init: function() {		
 		
 		App.init("VMain");
-		
+
     },
 	
 	// Events
 	VMain_onShow: function()
 	{
 		console.log('done.');
-	}	
+	},
+	scanner_onclick: function()
+	{
+		cordova.plugins.barcodeScanner.scan(
+			function (result) {
+				/*var host=result.text.split('://')[1].split(':')[0]+'.xip.io';
+				var port=result.text.split('://')[1].split(':')[1].split('/')[0];
+				var address=result.text.split('://')[1].split(':')[1].split('/')[1];
+				location.href='http://'+host+':'+port+'/'+address;
+				*/
+				location.href=result.text;
+			}, 
+			function (error) {
+			  alert("Scanning failed: " + error);
+			}
+		);
+	}
 		
 });
