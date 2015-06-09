299828485ce36e4d0019873cf3c2fc94361d6cd1
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2015-06-09 14:30:28 +0200

    Update# 5

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -1,3 +1,22 @@
+App.view.define('My.TapPanel', {
+    extend: 'Ext.Panel',
+	alias: "widget.TapPanel",
+	config: {
+		layout: {
+			type: 'fit'
+		},
+		listeners:[
+			{
+				element: 'element',
+				event: 'tap',
+				fn: function() {
+					this.fireEvent("tap", this);
+				}
+			}
+		]
+	}
+});
+
 App.view.define('VMain', {
 
 	extend: 'Ext.navigation.View',
@@ -19,11 +38,18 @@ App.view.define('VMain', {
 		},	
 		items: [
 			{
-				title: 'BLANK Project Mobile',
-				layout: 'fit',
+				title: 'Omneedia Scanner',
+				layout: {
+					type: 'vbox',
+					pack: 'center'                        
+                },
 				items: [
 					{
-						html: 'Hello world'
+						xtype: 'TapPanel',					
+						baseCls : "UIScanner",
+						itemId: "scanner",
+						width:"100%",
+						height:180
 					}
 				]
 			}
